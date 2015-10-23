32af85a32ff25d0f3b5117c4e412e8c428054675
dmdiehr <ddiehr@yahoo.com>
2015-10-22 23:18:33 -0700

    Add an little content to index.html to run the function. Tweaked the function and the tests to make it more useable in html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,38 @@
 var lyrics = function(input) {
   var output = '';
   var userInput = parseInt(input);
-  if (isNaN(userInput) || userInput<0){
-    return 'We need a non-negative number.';
+  if (isNaN(userInput) || userInput<0 || userInput>99) {
+    return 'Please enter a number between 0 and 99.';
   } else {
 
       for (var i=userInput; i>2 ; i--){
-        output = output + i + ' bottles of beer on the wall, ' + i + ' bottles of beer. Take one down and pass it around, ' + (i-1) + ' bottles of beer on the wall.<br>';
+        output = output +'<li>' + i + ' bottles of beer on the wall, ' + i + ' bottles of beer. Take one down and pass it around, ' + (i-1) + ' bottles of beer on the wall.</li>';
       };
       if (userInput>1){
-        output = output + '2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.<br>';
+        output = output + '<li>2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.</li>';
       }
 
       if (userInput>0){
-        output = output + '1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.<br>';
+        output = output + '<li>1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.</li>';
       }
 
-    output = output + "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
+    output = output + "<li>No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.</li>";
     return output;
   }
 }
 
+$(document).ready(function(){
+  $('#start-button').click(function(){
+    var input = $('#input').val();
+    $('#lyrics ul').html(lyrics(input));
+    $('#start').slideUp(2000);
 
 
 
+  });//end button click function
+}); //end document.ready 
 
-/////////////////////////////////////Attempt at Refactoring/////////////////////////////////////////////////////////
+/////////////////////////////Attempt at Refactoring lyrics() ////////////////////////////////
 
 // var lyrics = function(input) {
 //   String.prototype.insertTextAtIndices = function(text) {
@@ -68,5 +75,6 @@ var lyrics = function(input) {
 // }
 
 
-//In the end, the refactoring made the function more complicated.  It was an interesting exercise and may have been useful if there had been more than two
-// extra permutations of the pluralization. I'm going to keep it here for posterity.
+//In the end, the refactoring made the function more complicated.  It was an interesting exercise and may have been useful if there had been more than two extra permutations of the pluralization. I'm going to keep it here for posterity.
+
+// Note, after writing this function, I decided to change the output of lyrics to have each individual line of the lyric be represented by an <li>. This function will only pass the older tests that test for output with <br> tags (designed to be inserted into a single set of <p> tags.)
--- spec/specs.js
+++ spec/specs.js
@@ -1,34 +1,44 @@
 describe('lyrics', function(){
-  it("will not run if the input is either not a number or less than 0.", function(){
-    expect(lyrics('five')).to.equal('We need a non-negative number.');
+  it("will not run if the input is not a number.", function(){
+    expect(lyrics('five')).to.equal('Please enter a number between 0 and 99.');
+  });
+});
+describe('lyrics', function(){
+  it("will not run if the input is a negative number.", function(){
+    expect(lyrics(-5)).to.equal('Please enter a number between 0 and 99.');
+  });
+});
+describe('lyrics', function(){
+  it("will not run if the input is number greater than 99.", function(){
+    expect(lyrics(100)).to.equal('Please enter a number between 0 and 99.');
   });
 });
 
 describe('lyrics', function(){
   it("will run if the input is a string that can be converted to a number.", function(){
-    expect(lyrics('5')).to.not.equal('We need a non-negative number.');
+    expect(lyrics('5')).to.not.equal('We need a number between 0 and 99.');
   });
 });
 
 
 describe('lyrics', function(){
   it("will generate the correct final line.", function(){
-    expect(lyrics(3).slice(-128)).to.equal('No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.');
+    expect(lyrics(5).slice(-137)).to.equal('<li>No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.</li>');
   });
 });
 describe('lyrics', function(){
   it("will generate the correct(e.g. pluralization) second to last line.", function(){
-    expect(lyrics(3).slice(-250, -128)).to.equal('1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.<br>');
+    expect(lyrics(5).slice(-264, -137)).to.equal('<li>1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.</li>');
   });
 });
 describe('lyrics', function(){
   it("will generate the correct(e.g. pluralization) third to last line.", function(){
-    expect(lyrics(3).slice(-367, -250)).to.equal('2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.<br>');
+    expect(lyrics(5).slice(-386, -264)).to.equal('<li>2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.</li>');
   });
 });
 
 describe('lyrics', function(){
   it("will generate the lyrics, including line breaks, starting with the input value.", function(){
-    expect(lyrics(3)).to.equal('3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 2 bottles of beer on the wall.<br>2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.<br>1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.<br>No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.');
+    expect(lyrics(3)).to.equal('<li>3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 2 bottles of beer on the wall.</li><li>2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.</li><li>1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.</li><li>No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.</li>');
   });
 });
